4f49902e804a8bdbacc42f7c351e7dc5ac63bcaf
Ayush <ayushagarwal.dev@gmail.com>
2024-09-26 23:07:22 +0530

    add crossterm for raw mode input, aschii, binary and character logging code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, Read};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(val) => {
+                let ch = val as char;
+                if ch.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", val);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:?}\r", val, ch);
+                }
+                if ch == 'q' {
+                    break;
+                }
+            }
+            Err(err) => println!("Error: {}", err)
+        }
+    }
+    disable_raw_mode().unwrap();
 }
